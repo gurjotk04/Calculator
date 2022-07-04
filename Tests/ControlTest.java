c4fcc33ea70d104065102b83931874cd7502ae4b
gurjotk04 <107280304+gurjotk04@users.noreply.github.com>
2022-07-04 15:10:58 -0700

    Update ControlTest.java

--- Tests/ControlTest.java
+++ Tests/ControlTest.java
@@ -1,5 +1,16 @@
 package Tests;
 
 public class ControlTest {
+	
+	public void testSum() {
+		
+		Calculator calculator = new Calculator();
+		int result = calculator.sum(6, 2);
+		if (result != 8) 
+      {  // if 6 + 2 != 8
+			Assert.fail();
+		}
+	}
+
     
 }
